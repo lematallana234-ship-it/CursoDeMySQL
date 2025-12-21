90dba331576b1b3495fed88b041f4b16b44197a5
Roger Tallana <lematallana234@gmail.com>
2025-12-21 11:40:47 -0500

    Cambios

--- Consultas.sql
+++ Consultas.sql
@@ -170,3 +170,158 @@ JOIN medicinas mg
 
 WHERE mc.tipo = 'COM'
   AND mg.tipo = 'GEN';
+
+--19/12/2025
+---Crear todas las combinaciones posibles en las tablas 
+-- de clientes y la tabla de medicinafrecunete.
+-- Producto Carteciano 
+SELECT * FROM clientes, medicinas_frecuentes
+WHERE clientes.cedula=medicinas_frecuentes.cedula_cliente;
+  --- Producto carteciano entre 3 conjuntos
+SELECT 
+  c.cedula,
+  c.nombre,
+  m.nombre AS medicina,
+  mf.descuento,
+  m.tipo
+FROM  
+  medicinas_frecuentes mf
+JOIN clientes c ON c.cedula = mf.cedula_cliente
+JOIN medicinas m ON m.id = mf.id_medicina
+WHERE 
+  m.tipo = 'COM';
+
+
+SELECT
+  mcom.id, 
+  mcom.nombre,
+  mcom.precio,
+  mcg.alternativa,
+  mgen.nombre,
+  mgen.precio,
+  mgen.precio - mgen.precio as diferencia
+FROM
+  clasificacion_medicinas mcg
+JOIN medicinas mcom on mcom.id = mcg.id_medicina
+JOIN medicinas mgen on mgen.id = mcg.id_medicina
+;
+
+--- Caso: Presentar una fractura y sus detalles, que incluya,
+--        los datos de la farmacia: nombre, ruc, correo
+--        los datos del client:...
+--        los datos de la cabera de la factura: numero, fecha
+--        las medicinas vendidas: nombre de medicina, id, cantidad, precio, subtotal
+--        los datos al pie de la factura: Total y la forma de pago
+
+-- 1. Carga de datos en factura cabecera y detalle
+--    usar los datos ya existentes
+-- 2. selec para cabecera de factura
+-- 3. selec para los detalles de factura
+-- 4. selec para el pie factura.
+
+USE SaludTotal;
+
+-- =====================================
+-- 1. CARGA DE DATOS (CABECERA)
+-- =====================================
+INSERT INTO facturas
+VALUES ('0000000200', '2025-12-19', '1000000001', 0);
+
+INSERT INTO facturas
+VALUES ('0000000300', '2025-12-20', '1000000002', 0);
+-- =====================================
+-- 1. CARGA DE DATOS (DETALLE – 3 MEDICINAS)
+-- =====================================
+INSERT INTO facturadetalle VALUES
+('0000000200', 8, 2, 3.25),   -- Amoxicilina
+('0000000200', 9, 1, 8.50),   -- Azitromicina
+('0000000200', 10, 3, 1.80);  -- Diclofenaco
+INSERT INTO facturadetalle VALUES
+('0000000300', 11, 2, 2.40),   
+('0000000300', 12, 1, 0.95),   
+('0000000300', 13, 3, 0.60);
+
+-- =====================================
+-- ACTUALIZAR TOTAL DE FACTURA
+-- =====================================
+UPDATE facturas
+SET total = (
+    SELECT SUM(cantidad * precio)
+    FROM facturadetalle
+    WHERE facturanumero = '0000000200'
+)
+WHERE facturanumero = '0000000200';
+
+UPDATE facturas
+SET total = (
+    SELECT SUM(cantidad * precio)
+    FROM facturadetalle
+    WHERE facturanumero = '0000000300'
+)
+WHERE facturanumero = '0000000300';
+
+
+-- =====================================
+-- 2. CABECERA DE FACTURA
+-- =====================================
+SELECT
+    e.ruc,
+    e.direccion,
+    e.telefono,
+    e.email,
+    f.facturanumero,
+    f.fecha
+FROM facturas f
+CROSS JOIN empresa e
+WHERE f.facturanumero = '0000000200';
+
+-- =====================================
+-- 3. DETALLE DE FACTURA
+-- =====================================
+SELECT
+    m.id AS medicamento_id,
+    m.nombre AS medicamento,
+    fd.cantidad,
+    fd.precio,
+    (fd.cantidad * fd.precio) AS subtotal
+FROM facturadetalle fd
+JOIN medicinas m ON fd.medicamento_id = m.id
+WHERE fd.facturanumero = '0000000200';
+
+SELECT
+    m.id AS medicamento_id,
+    m.nombre AS medicamento,
+    fd.cantidad,
+    fd.precio,
+    (fd.cantidad * fd.precio) AS subtotal
+FROM facturadetalle fd
+JOIN medicinas m ON fd.medicamento_id = m.id
+WHERE fd.facturanumero = '0000000300'
+
+
+-- =====================================
+-- 4. PIE DE FACTURA
+-- =====================================
+SELECT
+    f.total,
+    'EFECTIVO' AS forma_pago
+FROM facturas f
+WHERE f.facturanumero = '0000000200';
+
+SELECT
+    f.total,
+    'Tarjeta' AS forma_pago
+FROM facturas f
+WHERE f.facturanumero = '0000000300';
+
+SELECT
+    SUM(fd.cantidad * fd.precio) AS subtotal
+FROM facturadetalle fd
+JOIN medicinas m ON fd.medicamento_id = m.id
+WHERE fd.facturanumero = '0000000300'
+
+SELECT
+    SUM(fd.cantidad * fd.precio) AS subtotal
+FROM facturadetalle fd
+JOIN medicinas m ON fd.medicamento_id = m.id
+WHERE fd.facturanumero = '0000000200';
